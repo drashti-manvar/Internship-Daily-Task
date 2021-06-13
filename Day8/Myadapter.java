1293bf534e81e785e82f397bd05953bfb38fe5cc
Manvar Drashti <84967882+drashti-manvar@users.noreply.github.com>
2021-06-13 19:06:33 +0530

    Add files via upload

---
+++ Day8/Myadapter.java
@@ -0,0 +1,55 @@
+package com.example.myapplication;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+
+public class Myadapter extends BaseAdapter {
+        int[] image;
+        String[] name;
+        LayoutInflater layoutInflater;
+        Context context;
+
+        public Myadapter(int[] image, String[] name, Context context) {
+            this.image = image;
+            this.name = name;
+            this.context = context;
+            layoutInflater=(LayoutInflater.from(context));
+        }
+
+        @Override
+        public int getCount() {
+            return name.length;
+        }
+
+        @Override
+        public Object getItem(int position) {
+            return null;
+        }
+
+        @Override
+        public long getItemId(int position) {
+            return 0;
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            layoutInflater.inflate(R.layout.row_list,null);
+
+            ImageView iv1=convertView.findViewById(R.id.iv1);
+            TextView tv1=convertView.findViewById(R.id.textview1);
+
+            tv1.setText(name[position]);
+            iv1.setImageResource(image[position]);
+
+            return convertView;
+
+
+
+        }
+    }
